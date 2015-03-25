cfb65be2351fdc25feaa8f0cbec4e0981127b47c
Alex <Alex@Alex.fritz.box>
2015-03-25 14:45:30 +0100

    Testprogramm welches hs-mannheim.de untersucht. Benötigt eine db mit namen crawler

--- SearchEngine/src/main/java/com/hsmannheim/arl/searchengine/Main.java
+++ SearchEngine/src/main/java/com/hsmannheim/arl/searchengine/Main.java
@@ -1,17 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.hsmannheim.arl.searchengine;
 
-/**
- *
- * @author Alex
- */
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Main {
-    
-    public static void main(String[]argu){
-        System.out.println("hello world ");
+
+    public static DB db = new DB();
+
+    public static void main(String[] args) throws SQLException, IOException {
+        db.runSql2("TRUNCATE Record;");
+        processPage("http://www.hs-mannheim.de");
+    }
+
+    public static void processPage(String URL) throws SQLException, IOException {
+        //check if the given URL is already in database
+        String sql = "select * from Record where URL = '" + URL + "'";
+        ResultSet rs = db.runSql(sql);
+        if (rs.next()) {
+
+        } else {
+            //store the URL to database to avoid parsing again
+            sql = "INSERT INTO  `Crawler`.`Record` " + "(`URL`) VALUES " + "(?);";
+            PreparedStatement stmt = db.conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, URL);
+            stmt.execute();
+
+            //get useful information
+            Document doc = Jsoup.connect("http://www.hs-mannheim.de").timeout(10 * 1000).get();
+
+            System.out.println(URL);
+
+            //get all links and recursively call the processPage method
+            Elements questions = doc.select("a[href]");
+            System.out.println(questions.size());
+            for (Element link : questions) {
+                System.out.println(link.attr("href"));
+
+                processPage(link.attr("abs:href"));
+
+            }
+        }
+
     }
 }
