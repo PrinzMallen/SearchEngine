9ce6b56742ae1b00ed6baf18861394e9723f1382
Alex <Alex@Alex.fritz.box>
2015-03-25 13:46:11 +0100

    mh

---
+++ SearchEngine/src/main/java/com/hsmannheim/arl/searchengine/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hsmannheim.arl.searchengine;
+
+/**
+ *
+ * @author Alex
+ */
+public class Main {
+    
+    public static void main(String[]argu){
+        System.out.println("hello world ");
+    }
+}
