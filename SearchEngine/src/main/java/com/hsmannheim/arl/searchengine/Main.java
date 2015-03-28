014c2ee6b4d4112b51499d5fd6daf35f2bb261ac
Alex <Alex@Alex.fritz.box>
2015-03-28 15:39:14 +0100

    Main enthält ein "minimal Beispiel". Keine Datenbank. Es werden txt für jede Seite angelegt. Unter /target. Dann Index angelegt und suche ermöglicht. Keine Datenbank oder so, Index etc muss auch immer neu erstellt werden atm. --> Crawler extrem langsam. Indizierung und Suche sehr schnell!

--- SearchEngine/src/main/java/com/hsmannheim/arl/searchengine/Main.java
+++ SearchEngine/src/main/java/com/hsmannheim/arl/searchengine/Main.java
@@ -1,10 +1,43 @@
 package com.hsmannheim.arl.searchengine;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.LongField;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,40 +46,348 @@ import org.jsoup.select.Elements;
 public class Main {
 
     public static DB db = new DB();
+    static int i = 0;
+    static String indexPath = "target\\index";
+    static String docsPath = "target\\files\\";
+    static Map<String,String> urls=new HashMap<String, String>();
 
-    public static void main(String[] args) throws SQLException, IOException {
-        db.runSql2("TRUNCATE Record;");
+    public static void main(String[] args) throws SQLException, IOException, ParseException {
+        long start=System.currentTimeMillis();
         processPage("http://www.hs-mannheim.de");
+        System.out.println("Crawler took "+(System.currentTimeMillis()-start)+"ms");
+        createIndex();
+        seachFiles();
+    }
+
+    public static void createIndex() {
+
+        boolean create = true;
+        final Path docDir = Paths.get(docsPath);
+//        if (!Files.isReadable(docDir)) {
+//            System.out.println("Document directory '" + docDir.toAbsolutePath() + "' does not exist or is not readable, please check the path");
+//            System.exit(1);
+//        }
+
+        Date start = new Date();
+        try {
+            System.out.println("Indexing to directory '" + indexPath + "'...");
+
+            Directory dir = FSDirectory.open(Paths.get(indexPath));
+            Analyzer analyzer = new StandardAnalyzer();
+            IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+
+            if (create) {
+                // Create a new index in the directory, removing any
+                // previously indexed documents:
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+            } else {
+                // Add new documents to an existing index:
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+            }
+
+            // Optional: for better indexing performance, if you
+            // are indexing many documents, increase the RAM
+            // buffer.  But if you do this, increase the max heap
+            // size to the JVM (eg add -Xmx512m or -Xmx1g):
+            //
+            // iwc.setRAMBufferSizeMB(256.0);
+            IndexWriter writer = new IndexWriter(dir, iwc);
+            indexDocs(writer, docDir);
+
+            // NOTE: if you want to maximize search performance,
+            // you can optionally call forceMerge here.  This can be
+            // a terribly costly operation, so generally it's only
+            // worth it when your index is relatively static (ie
+            // you're done adding documents to it):
+            //
+            // writer.forceMerge(1);
+            writer.close();
+
+            Date end = new Date();
+            System.out.println(end.getTime() - start.getTime() + " total milliseconds for indexing");
+
+        } catch (IOException e) {
+            System.out.println(" caught a " + e.getClass()
+                    + "\n with message: " + e.getMessage());
+        }
     }
 
     public static void processPage(String URL) throws SQLException, IOException {
-        //check if the given URL is already in database
-        String sql = "select * from Record where URL = '" + URL + "'";
-        ResultSet rs = db.runSql(sql);
-        if (rs.next()) {
-
-        } else {
-            //store the URL to database to avoid parsing again
-            sql = "INSERT INTO  `Crawler`.`Record` " + "(`URL`) VALUES " + "(?);";
-            PreparedStatement stmt = db.conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, URL);
-            stmt.execute();
+            if(!new File(docsPath).exists()){
+                new File(docsPath).mkdir();
+            }
+           
+       
 
             //get useful information
             Document doc = Jsoup.connect("http://www.hs-mannheim.de").timeout(10 * 1000).get();
 
-            System.out.println(URL);
+            Writer writer = null;
 
+            i++;
+            //save as text as txt
+            try {
+                File file = new File(docsPath + i + ".txt");
+                BufferedWriter output = new BufferedWriter(new FileWriter(file));
+                output.write(doc.text());
+                output.close();
+                urls.put(URL, i+"");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             //get all links and recursively call the processPage method
             Elements questions = doc.select("a[href]");
-            System.out.println(questions.size());
             for (Element link : questions) {
-                System.out.println(link.attr("href"));
 
-                processPage(link.attr("abs:href"));
+                if(!urls.containsKey(link.attr("abs:href"))){
+                    processPage(link.attr("abs:href"));
+                }
 
             }
         }
 
+    
+
+    /**
+     * Indexes the given file using the given writer, or if a directory is
+     * given, recurses over files and directories found under the given
+     * directory.
+     *
+     * NOTE: This method indexes one document per input file. This is slow. For
+     * good throughput, put multiple documents into your input file(s). An
+     * example of this is in the benchmark module, which can create "line doc"
+     * files, one document per line, using the
+     * <a href="../../../../../contrib-benchmark/org/apache/lucene/benchmark/byTask/tasks/WriteLineDocTask.html"
+     * >WriteLineDocTask</a>.
+     *
+     * @param writer Writer to the index where the given file/dir info will be
+     * stored
+     * @param path The file to index, or the directory to recurse into to find
+     * files to index
+     * @throws IOException If there is a low-level I/O error
+     */
+    static void indexDocs(final IndexWriter writer, Path path) throws IOException {
+        if (Files.isDirectory(path)) {
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    try {
+                        indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
+                    } catch (IOException ignore) {
+                        // don't index files that can't be read.
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } else {
+            indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
+        }
+    }
+
+    /**
+     * Indexes a single document
+     */
+    static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
+        try (InputStream stream = Files.newInputStream(file)) {
+            // make a new, empty document
+            org.apache.lucene.document.Document doc = new org.apache.lucene.document.Document();
+
+      // Add the path of the file as a field named "path".  Use a
+            // field that is indexed (i.e. searchable), but don't tokenize 
+            // the field into separate words and don't index term frequency
+            // or positional information:
+            Field pathField = new StringField("path", file.toString(), Field.Store.YES);
+            doc.add(pathField);
+
+      // Add the last modified date of the file a field named "modified".
+            // Use a LongField that is indexed (i.e. efficiently filterable with
+            // NumericRangeFilter).  This indexes to milli-second resolution, which
+            // is often too fine.  You could instead create a number based on
+            // year/month/day/hour/minutes/seconds, down the resolution you require.
+            // For example the long value 2011021714 would mean
+            // February 17, 2011, 2-3 PM.
+            doc.add(new LongField("modified", lastModified, Field.Store.NO));
+
+      // Add the contents of the file to a field named "contents".  Specify a Reader,
+            // so that the text of the file is tokenized and indexed, but not stored.
+            // Note that FileReader expects the file to be in UTF-8 encoding.
+            // If that's not the case searching for special characters will fail.
+            doc.add(new TextField("contents", new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+
+            if (writer.getConfig().getOpenMode() == IndexWriterConfig.OpenMode.CREATE) {
+                // New index, so we just add the document (no old document can be there):
+                System.out.println("adding " + file);
+                writer.addDocument(doc);
+            } else {
+        // Existing index (an old copy of this document may have been indexed) so 
+                // we use updateDocument instead to replace the old one matching the exact 
+                // path, if present:
+                System.out.println("updating " + file);
+                writer.updateDocument(new Term("path", file.toString()), doc);
+            }
+        }
+    }
+
+    public static void seachFiles() throws IOException, ParseException {
+
+     
+        String field = "contents";
+        String queries = null;
+        int repeat = 0;
+        boolean raw = false;
+        String queryString = null;
+        int hitsPerPage = 10;
+
+        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
+        IndexSearcher searcher = new IndexSearcher(reader);
+        Analyzer analyzer = new StandardAnalyzer();
+
+        BufferedReader in = null;
+        if (queries != null) {
+            in = Files.newBufferedReader(Paths.get(queries), StandardCharsets.UTF_8);
+        } else {
+            in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+        }
+        QueryParser parser = new QueryParser(field, analyzer);
+        while (true) {
+            if (queries == null && queryString == null) {                        // prompt the user
+                System.out.println("Enter query: ");
+            }
+
+            String line = queryString != null ? queryString : in.readLine();
+
+            if (line == null || line.length() == -1) {
+                break;
+            }
+
+            line = line.trim();
+            if (line.length() == 0) {
+                break;
+            }
+
+            Query query = parser.parse(line);
+            System.out.println("Searching for: " + query.toString(field));
+
+            if (repeat > 0) {                           // repeat & time as benchmark
+                Date start = new Date();
+                for (int i = 0; i < repeat; i++) {
+                    searcher.search(query, null, 100);
+                }
+                Date end = new Date();
+                System.out.println("Time: " + (end.getTime() - start.getTime()) + "ms");
+            }
+
+            doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
+
+            if (queryString != null) {
+                break;
+            }
+        }
+        reader.close();
+    }
+
+    /**
+     * This demonstrates a typical paging search scenario, where the search
+     * engine presents pages of size n to the user. The user can then go to the
+     * next page if interested in the next hits.
+     *
+     * When the query is executed for the first time, then only enough results
+     * are collected to fill 5 result pages. If the user wants to page beyond
+     * this limit, then the query is executed another time and all hits are
+     * collected.
+     *
+     */
+    public static void doPagingSearch(BufferedReader in, IndexSearcher searcher, Query query,
+            int hitsPerPage, boolean raw, boolean interactive) throws IOException {
+        long startTime=System.currentTimeMillis();
+        // Collect enough docs to show 10 pages
+        TopDocs results = searcher.search(query, 10 * hitsPerPage);
+        ScoreDoc[] hits = results.scoreDocs;
+        System.out.println("Query took "+(System.currentTimeMillis()-startTime));
+        int numTotalHits = results.totalHits;
+        System.out.println(numTotalHits + " total matching documents");
+
+        int start = 0;
+        int end = Math.min(numTotalHits, hitsPerPage);
+
+        while (true) {
+            if (end > hits.length) {
+                System.out.println("Only results 1 - " + hits.length + " of " + numTotalHits + " total matching documents collected.");
+                System.out.println("Collect more (y/n) ?");
+                String line = in.readLine();
+                if (line.length() == 0 || line.charAt(0) == 'n') {
+                    break;
+                }
+
+                hits = searcher.search(query, numTotalHits).scoreDocs;
+            }
+
+            end = Math.min(hits.length, start + hitsPerPage);
+
+            for (int i = start; i < end; i++) {
+                if (raw) {                              // output raw format
+                    System.out.println("doc=" + hits[i].doc + " score=" + hits[i].score);
+                    continue;
+                }
+
+                org.apache.lucene.document.Document doc = searcher.doc(hits[i].doc);
+                String path = doc.get("path");
+                if (path != null) {
+                    System.out.println((i + 1) + ". " + path);
+                    String title = doc.get("title");
+                    if (title != null) {
+                        System.out.println("   Title: " + doc.get("title"));
+                    }
+                } else {
+                    System.out.println((i + 1) + ". " + "No path for this document");
+                }
+
+            }
+
+            if (!interactive || end == 0) {
+                break;
+            }
+
+            if (numTotalHits >= end) {
+                boolean quit = false;
+                while (true) {
+                    System.out.print("Press ");
+                    if (start - hitsPerPage >= 0) {
+                        System.out.print("(p)revious page, ");
+                    }
+                    if (start + hitsPerPage < numTotalHits) {
+                        System.out.print("(n)ext page, ");
+                    }
+                    System.out.println("(q)uit or enter number to jump to a page.");
+
+                    String line = in.readLine();
+                    if (line.length() == 0 || line.charAt(0) == 'q') {
+                        quit = true;
+                        break;
+                    }
+                    if (line.charAt(0) == 'p') {
+                        start = Math.max(0, start - hitsPerPage);
+                        break;
+                    } else if (line.charAt(0) == 'n') {
+                        if (start + hitsPerPage < numTotalHits) {
+                            start += hitsPerPage;
+                        }
+                        break;
+                    } else {
+                        int page = Integer.parseInt(line);
+                        if ((page - 1) * hitsPerPage < numTotalHits) {
+                            start = (page - 1) * hitsPerPage;
+                            break;
+                        } else {
+                            System.out.println("No such page");
+                        }
+                    }
+                }
+                if (quit) {
+                    break;
+                }
+                end = Math.min(numTotalHits, start + hitsPerPage);
+            }
+        }
     }
 }
